3572a3993191ecc0b92615897693d05969b170c0
L1B4RDO <80779406+L1B4RDO@users.noreply.github.com>
2021-05-30 21:38:37 -0500

    Se agrega restricción
    ** Se agrega restringe que se puedan ingresar dos vehículos con la misma placa

--- js/form.js
+++ js/form.js
@@ -1,12 +1,26 @@
+// ############ VARIABLES ##################
+
+let placas='';
+let bandera = 'true';
+let placasRegistradas='';
+let nombres='';
+let apellidos='';
+let correo='';
+let estados='';
+let vehiculo='';
+let fechaRegister='';
+
+// ################## END ##################
+
 const taskForm = document.querySelector('#task-form'); // capturo lo que tengo en task-form  todos los input 
 
 const fecha = new Date();
-const FechaSalida = fecha.getFullYear()+'-'+fecha.getMonth()+'-'+fecha.getDate()+' '+fecha.getHours()+':'+fecha.getMinutes()+':'+fecha.getSeconds();
+const FechaSalida = fecha.getFullYear() + '-' + fecha.getMonth() + '-' + fecha.getDate() + ' ' + fecha.getHours() + ':' + fecha.getMinutes() + ':' + fecha.getSeconds();
 //console.log(FechaSalida);
 
 let editStatus = false;
 //funcion para guardar una tarea 
-const saveTask = (nombre, apellido, email, estado, mensaje, vehiculo,fecha) =>
+const saveTask = (nombre, apellido, email, estado, mensaje, vehiculo, fecha) =>
     db.collection('Parqueo').doc().set({//indico que quiero guardar en la coleccion tasks un documento y que ese documento va a tener unos objetos
         Name: nombre,
         LastName: apellido,
@@ -18,24 +32,54 @@ const saveTask = (nombre, apellido, email, estado, mensaje, vehiculo,fecha) =>
     });
 
 //con el evento submit guardamos en firebase 
+
+
 taskForm.addEventListener('submit', async (evento) => {
-    
     evento.preventDefault(); //para que no se refresque la pagina 
     const nombre = taskForm['nombre']; //capturo de esta manera capturo lo que tengo en mi input task-tiempo capturo el elemento
+    nombres = nombre.value;
     const apellido = taskForm['apellidos'];
+    apellidos = apellido.value;
     const email = taskForm['email'];
+    correo = email.value;
     const estado = taskForm['estado'];
+    estados = estado.value;
     const mensaje = taskForm['mensaje'];
+    placasRegistradas = mensaje.value;
     const select = taskForm['task-select'];
+    vehiculo = select.value;
     const fecha = taskForm['fecha'];
-    //console.log(nombre)
-    if (!editStatus) {
-        //en el evento submit llamo a mi funcon savetask y le envio el titulo y la descripcion con el .value para guardar el valor
-        await saveTask(nombre.value, apellido.value, email.value, estado.value, mensaje.value, select.value,fecha.value);
-    };
-    alert('Usuario Registrado con exito')
-    taskForm.reset();
+    fechaRegister = fecha.value;
+    // Con esto valido que no se ingrese el mismo vehiculo dos veces 
+    const data = db.collection('Parqueo')
+        .get()
+        .then((snapshot) => {
+            //console.log('Data', snapshot.docs)
+            // console.log('User', snapshot.docs)
+            const data = snapshot.docs
+
+            data.forEach(doc => {
+                const post = doc.data();//Con esto traigo la data de mi database
+                post.id = doc.id;
+                placas = post.Message;
+                if (placas == placasRegistradas) {
+                    bandera = 'false';
+                }
+                console.log(placas,'=',placasRegistradas)
+            });
+            if (bandera == 'true') {
+                saveTask(nombres, apellidos, correo, estados, placasRegistradas, vehiculo, fechaRegister);
+                alert('Usuario Registrado con exito')
     
+            } else {
+                alert('Este vehiculo ya se encuentra reguistrado')
+                bandera = 'true';
+            }
+        });
+ 
+ 
+    taskForm.reset();
+
     nombre.focus();
 
 
@@ -54,7 +98,7 @@ auth.onAuthStateChanged(user => {
         }
 
         users_spa.innerHTML += li;
-       
+
     } else {
         console.log('sign out')
 
